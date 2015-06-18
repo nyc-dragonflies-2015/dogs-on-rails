98c9e727623888800e6268b723eddb388f3645ff
Steven G. Harms <steven@devbootcamp.com>
2015-06-18 15:02:05 -0400

    Add JS to fetch a new Dog form; creating it

---
+++ app/assets/javascripts/dogs.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+  $("#add-dog").on('click', function(event) {
+    $.ajax("http://localhost:3000/dogs/form")
+      .done(function(formHtml) {
+        $("body").append(formHtml);
+        $("form").on('submit', function(e) {
+          e.preventDefault();
+
+          $.ajax("http://localhost:3000/dogs", {
+            method: "POST",
+            data: $("form").serialize()
+          })
+          .done(function(dogPartial) {
+
+          })
+          .fail(function(){
+            console.log('zack panda');
+          });
+        });
+      })
+      .fail(function() {
+        console.log("o shit, son");
+      });
+  });
+});
