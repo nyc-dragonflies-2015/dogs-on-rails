798524b66b0cea8a4054359340dc2721f38ac8c3
Steven G. Harms <steven@devbootcamp.com>
2015-06-18 15:13:07 -0400

    User: add model

---
+++ app/models/user.rb
@@ -0,0 +1,6 @@
+require 'bcrypt'
+class User < ActiveRecord::Base
+  has_secure_password
+  validates :username, uniqueness: true, presence: true
+  validates :password_digest, presence: true
+end
---
+++ db/migrate/20150615210352_change_password_hash_column_to_password_digest.rb
@@ -0,0 +1,5 @@
+class ChangePasswordHashColumnToPasswordDigest < ActiveRecord::Migration
+  def change
+    rename_column :users, :password_hash, :password_digest
+  end
+end
